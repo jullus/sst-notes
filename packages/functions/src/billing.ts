3bb1014d2379b269dab255f7c14113440312e971
Iulian Vlad <iulian.vlad86@gmail.com>
2023-12-08 13:40:08 +0200

    Add billing API

---
+++ packages/functions/src/billing.ts
@@ -0,0 +1,23 @@
+import handler from '@notes/core/handler';
+import {calculateCost} from '@notes/core/cost';
+import Stripe from 'stripe';
+import {Config} from 'sst/node/config';
+
+export const main = handler(async (event) => {
+    const {storage, source} = JSON.parse(event.body || '{}');
+    const amount = calculateCost(storage);
+    const description = 'Scratch charge';
+
+    //Load the secret key
+    const stripe = new Stripe(Config.STRIPE_SECRET_KEY, {
+        apiVersion: '2023-10-16',
+    });
+
+    await stripe.charges.create({
+        source,
+        amount,
+        description,
+        currency: 'usd',
+    });
+    return JSON.stringify({status: true})
+});

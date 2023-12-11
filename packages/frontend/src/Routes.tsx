280f7182994fb39eb6beccf55b0edaa8c726063f
Iulian Vlad <iulian.vlad86@gmail.com>
2023-12-11 06:36:44 +0200

    add homepage

---
+++ packages/frontend/src/Routes.tsx
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./containers/Home.tsx";
+export default function Links() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
+}

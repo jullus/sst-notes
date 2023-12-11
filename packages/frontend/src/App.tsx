ff16835c37e8ad061e1f9629d556c57154f28adf
Iulian Vlad <iulian.vlad86@gmail.com>
2023-12-11 06:46:46 +0200

    add not-found

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,17 +2,24 @@ import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./Routes.tsx";
 import Nav from "react-bootstrap/Nav";
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
     return (
         <div className="App container py-3">
             <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-                <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Nav>
-                        <Nav.Link href="/signup">Signup</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav activeKey={window.location.pathname}>
+                        <LinkContainer to="/signup">
+                            <Nav.Link>Signup</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
